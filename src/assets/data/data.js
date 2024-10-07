db43495ac665f75cb30e45cdaec6575078750d38
adenanma <38765175+adenanma@users.noreply.github.com>
2024-10-07 21:13:35 +0700

    Add files via upload

---
+++ src/assets/data/data.js
@@ -0,0 +1,126 @@
+export const data = {
+    bride: {
+        L: {
+            id: 1,
+            name: 'Adenan',
+            child: 'Putra ke lorem',
+            father: 'Lorem',
+            mother: 'Ipsum',
+            image: './src/assets/images/cowo.jpeg'
+        },
+        P: {
+            id: 2,
+            name: 'Acung',
+            child: 'Putri ke lorem',
+            father: 'Lorem',
+            mother: 'Ipsum',
+            image: './src/assets/images/cowo.jpeg'
+        },
+
+        couple: './src/assets/images/couple.png'
+    },
+
+    time: {
+        marriage: {
+            year: '2024',
+            month: 'November',
+            date: '14',
+            day: 'Kamis',
+            hours: {
+                start: '08.00',
+                finish: 'Selesai'
+            }
+        },
+        reception: {
+            year: '2024',
+            month: 'November',
+            date: '14',
+            day: 'Kamis',
+            hours: {
+                start: '11.00',
+                finish: 'Selesai'
+            }
+        },
+        address: 'QV68+C9F, RT.14/RW.6, Bidara Cina, Kecamatan Jatinegara, Kota Jakarta Timur, Daerah Khusus Ibukota Jakarta 13330'
+    },
+
+    link: {
+        calendar: 'https://calendar.app.google/sZuPoeZejuhSmmbi7',
+        map: 'https://maps.app.goo.gl/AiTHFeT48sXKyVhCA',
+    },
+
+    galeri: [
+        {
+            id: 1,
+            image: './src/assets/images/1.png'
+        },
+        {
+            id: 2,
+            image: './src/assets/images/2.png'
+        },
+        {
+            id: 3,
+            image: './src/assets/images/3.png'
+        },
+        {
+            id: 4,
+            image: './src/assets/images/4.png'
+        },
+        {
+            id: 5,
+            image: './src/assets/images/5.png'
+        }
+    ],
+
+    bank: [
+        {
+            id: 1,
+            name: 'Muhammad Adenan',
+            icon: './src/assets/images/bca.png',
+            rekening: '4960458856'
+        },
+        {
+            id: 2,
+            name: 'Muhammad Adenan',
+            icon: './src/assets/images/bca.png',
+            rekening: '4960458856'
+        },
+    ],
+
+    audio: './src/assets/audio/wedding.mp3',
+
+    api: 'https://script.google.com/macros/s/AKfycby7JimLPwjMZtZR5234an8gTWPYNkub0OOuNa40GEeQ982yDYfS61W_KCH6ZNRXy5zPPw/exec',
+
+    navbar: [
+        {
+            id: 1,
+            teks: 'Home',
+            icon: 'bx bxs-home-heart',
+            path: '#home',
+        },
+        {
+            id: 2,
+            teks: 'Mempelai',
+            icon: 'bx bxs-group',
+            path: '#bride',
+        },
+        {
+            id: 3,
+            teks: 'Tanggal',
+            icon: 'bx bxs-calendar-check',
+            path: '#time',
+        },
+        {
+            id: 4,
+            teks: 'Galeri',
+            icon: 'bx bxs-photo-album',
+            path: '#galeri',
+        },
+        {
+            id: 5,
+            teks: 'Ucapan',
+            icon: 'bx bxs-message-rounded-dots',
+            path: '#wishas',
+        },
+    ],
+}
